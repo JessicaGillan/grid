041101950773b43285b69535f4b0ce5fcf8229c8
JGilly <jlgillan@comcast.net>
2016-05-03 20:36:37 -0600

    Updated grid with button for user entry, hover color change functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
+
+defaultSize = 16;
+var screenWidth = $(window).width() - 10;
+console.log(screenWidth);
+
 $(document).ready( function(){
-	numberOfRows = 16;
-	numberofCols = 16;
-	for (var i = 0; i < (numberofCols * numberOfRows); i++) {
+	drawGrid(defaultSize);
+})
+
+function getUserEntry(){
+	clearGrid();
+	var newNumberOfColumns = prompt("Hello!\n How many squares do you want per side, bud?", defaultSize);
+    if (newNumberOfColumns > 0 && newNumberOfColumns < 500){
+    	numberofCols = newNumberOfColumns;
+    }else{
+    	numberofCols = defaultSize;
+    }
+    drawGrid(numberofCols);
+}
+
+function drawGrid(gridSize){
+	// Add divs as children of .container
+	for (var i = 0; i < (gridSize * gridSize); i++) {
 		$(".container").append("<div></div>");
 	};
 
-})
+	// Must create divs before you can add styling to them
+	// Always start with generic and work towards specific to avoid override!!
+	divWidth = screenWidth/gridSize - 2;
+	$("div").width( String(divWidth) + "px" );
+	$("div").height( String(divWidth) + "px" );
+	$(".container").width( String(screenWidth) + "px" );
+	$(".container").height( String(screenWidth) + "px" );
+
+	// Does not work when declared outside ready. Must be immedaitely applied to existing divs when outside?
+	$(".container > div").hover( function(){
+		$(this).addClass("changed");
+	})
+}
+
+function clearGrid(){
+	// $(".container > div").remove();
+	$(".container").empty();
+}
+
+
+
+
+
